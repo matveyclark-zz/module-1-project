8a9ec77f720ae183a3e70262d8dae2b702c66cb5
ana <anameciarobalo@gmail.com>
2019-12-02 10:41:29 +0000

    add venue class

--- get_requester.rb
+++ get_requester.rb
@@ -7,7 +7,22 @@ api_key = ENV["API_KEY"]
 page_num = "5"
 url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=#{api_key}&city=london&size=100&page=#{page_num}"
 
+
 uri = URI.parse(url)
 response = Net::HTTP.get_response(uri)
 json_response = JSON.parse(response.body)
-ap json_response
+json_response
+
+events = json_response["_embedded"]["events"] 
+
+VENUE_NAMES =[]
+events.each do |event|
+    event["_embedded"]["venues"].each do |venue|
+        VENUE_NAMES << venue["name"]
+    end 
+end 
+
+
+
+
+
