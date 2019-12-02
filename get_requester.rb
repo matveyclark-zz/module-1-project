4ceed922a40a416479eb974f56cf83856350a5b5
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-12-02 10:06:00 +0000

    Attractions class

--- get_requester.rb
+++ get_requester.rb
@@ -2,12 +2,19 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 require 'awesome_print'
+require 'pry'
 
-api_key = ENV["API_KEY"]
-page_num = "5"
+api_key = "4WKCVApBKneEgGkReH1BHrkIjLHpPedx"
+page_num = "1"
 url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=#{api_key}&city=london&size=100&page=#{page_num}"
 
 uri = URI.parse(url)
 response = Net::HTTP.get_response(uri)
 json_response = JSON.parse(response.body)
 ap json_response
+
+NAMES_ARRAY = []
+data = json_response["_embedded"]["events"]
+data.each { |hash| NAMES_ARRAY.push(hash["name"]) }
+
+# binding.pry
---
+++ lib/models/attractions.rb
@@ -0,0 +1,20 @@
+require_relative '../../get_requester.rb'
+require 'pry'
+
+class Attraction
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+end
+
+NAMES_ARRAY.uniq.each { |name| Attraction.new(name) }
